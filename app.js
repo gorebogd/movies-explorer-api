60d23df0f14d05d52f8670cf1e7a92bb8c274031
gorebogd <gorebogd@yandex.ru>
2021-04-14 14:56:37 +0300

    added mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
