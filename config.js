c2775e4925030858447db2b736fc0bd974927e24
gorebogd <gorebogd@yandex.ru>
2021-05-07 17:48:45 +0300

    jwt fix

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 const { MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { JWT_SECRET } = process.env;
 
 module.exports = {
   PORT,
   MONGO_URL,
+  JWT_SECRET,
 };
